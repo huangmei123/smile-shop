9c2718571dfc0ec925ed17b2488f750569714655
huangmei123 <1593702111@qq.com>
2020-05-01 14:50:09 +0800

    移动电商

--- smile-shop/src/components/pages/ShoppingMall.vue
+++ smile-shop/src/components/pages/ShoppingMall.vue
@@ -18,37 +18,87 @@
         <div class="swiper-area">
             <van-swipe :autoplay="1000">
                 <van-swipe-item v-for="( banner ,index) in bannerPicArray" :key="index" >
-                    <img :src="banner.imageUrl" width="100%"/>
+                    <img v-lazy="banner.image" width="100%"/>
                 </van-swipe-item>
             </van-swipe>
         </div>
+
+        <!-- type-bar -->
+        <div class="type-bar">
+            <div v-for="(cate,index) in category" :key="index">
+                <img v-lazy="cate.image" width="90%">
+                <span>{{cate.mallCategoryName}}</span>
+
+            </div>
+        </div>
+        <!-- 广告条 -->
+        <div>
+            <img v-lazy="adBanner" width="100%" />
+        </div>
+
+        <!-- 商品推荐 -->
+        <div class="recommend-area">
+            <div class="recommend-title">
+                商品推荐
+            </div>
+            <div class="recommend-body">
+                <swiper :options="swiperOption">
+                   <swiper-slide v-for="(item,index) in recommendGoods " :key="index" >
+                       <div class="recommend-item">
+                           <img :src="item.image" width="80%">
+                           <div>{{item.goodsName}}</div>
+                           <div>￥{{item.price | moneyFilter}}(￥{{item.mallPrice  | moneyFilter}})</div>
+                       </div>
+                   </swiper-slide> 
+                </swiper>
+            </div>
+        </div>
+        <swiperDefault></swiperDefault>
     </div>
+    
 </template>
 
 <script>
  import axios from 'axios'
+ import SwiperDefault from '../swiper/swiperDefault'
+ import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+ import 'swiper/css/swiper.css'
+
     export default {
         data() {
             return {
+                swiperOption:{
+                    slidesPerView:3
+                },
                 msg:'Shopping Mall',
                 locationIcon: require('../../assets/images/location.png'),
-                bannerPicArray:[
-                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
-                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
-                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
-                ],
+                bannerPicArray:[],
+                category:[],
+                adBanner:'',//广告条
+                recommendGoods:[],
+                // floor1:[],
+                // floor2:[],
+                // floor3:[],
+                // floorName:{},
             }
         },
-        cteated(){
+        components:{Swiper,SwiperSlide,SwiperDefault},
+        created(){
             axios({
                 url:'https://www.easy-mock.com/mock/5eab84dc0a92d726eae93bf2/SmileVue/index',
                 method:'get',
             })
             .then(response=>{
-                console.log(response)
+                console.log(response,'-------')
+                if(response.status==200){
+                    this.category=response.data.data.category;
+                    this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
+                    this.bannerPicArray= response.data.data.slides;
+                    this.recommendGoods = response.data.data.recommend;
+                }
             })
             .catch(error=>{
-                console.log(error)
+                console.log(error,'++++++++')
             });
             
         }
@@ -56,7 +106,7 @@
 </script>
 
 <style  scoped>
-    .search-bar{
+     .search-bar{
         height:2.2rem;
         background-color: #e5017d;
         line-height: 2.2rem;
@@ -81,4 +131,52 @@
         max-height:15rem;
         overflow: hidden;
     }
+
+    .type-bar{
+        background-color: #fff;
+        margin:0 .3rem .3rem .3rem;
+        border-radius: .3rem;
+        font-size:14px;
+        display:flex;
+        flex-direction:row;
+        flex-wrap:nowrap;
+    }
+    .type-bar div{
+        padding:.3rem;
+        font-size:12px;
+        text-align: center;
+        flex:1;
+    }
+    .recommend-area{
+        background-color: #fff;
+        margin-top:.3rem;
+    }
+    .recommend-title{
+        border-bottom:1px solid #eee;
+        font-size:14px;
+        padding:.2rem;
+        color:#e5017d;
+    }
+    .recommend-body{
+        border-bottom:1px solid #eee;
+    }
+    .recommend-item{
+        width:99%;
+        border-right:1px solid #eee;
+        font-size:12px;
+        text-align: center;
+    }
+    .hot-area{
+        text-align: center;
+        font-size:14px;
+        height: 1.8rem;
+        line-height:1.8rem;
+    }
+    .hot-goods{
+        height: 130rem;
+        overflow: hidden;
+        background-color: #fff;
+    }
+    
+
 </style>
--- smile-shop/src/main.js
+++ smile-shop/src/main.js
@@ -5,6 +5,11 @@ import App from './App'
 import router from './router'
 import {Button, Row, Col, Swipe, SwipeItem , Lazyload } from 'vant'
 
+// import vueSwiper from 'vue-awesome-swiper'
+/* 样式的话，我这里有用到分页器，就在全局中引入了样式 */
+// import 'swiper/dist/css/swiper.css'
+// Vue.use(vueSwiper);
+
 Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload)
 
 
