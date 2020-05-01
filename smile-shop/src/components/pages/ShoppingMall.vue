6e3163845c8cdf7e1dacfb287a55123d85d9e359
huangmei123 <1593702111@qq.com>
2020-05-01 10:17:31 +0800

    移动电商

---
+++ smile-shop/src/components/pages/ShoppingMall.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <!-- 搜索条 -->
+        <div class="search-bar">
+            <van-row>
+                <van-col span="3">
+                    <img :src="locationIcon" width="80%" class="location-icon"/>
+                </van-col>
+                <van-col span="16">
+                    <input type="text" class="search-input" />
+                </van-col>
+                <van-col span="5">
+                    <van-button size="mini">查找</van-button>
+                </van-col>
+            </van-row>
+        </div>
+        <!-- 轮播图 -->
+        <div class="swiper-area">
+            <van-swipe :autoplay="1000">
+                <van-swipe-item v-for="( banner ,index) in bannerPicArray" :key="index" >
+                    <img :src="banner.imageUrl" width="100%"/>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+    </div>
+</template>
+
+<script>
+ import axios from 'axios'
+    export default {
+        data() {
+            return {
+                msg:'Shopping Mall',
+                locationIcon: require('../../assets/images/location.png'),
+                bannerPicArray:[
+                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
+                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
+                    {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
+                ],
+            }
+        },
+        cteated(){
+            axios({
+                url:'https://www.easy-mock.com/mock/5eab84dc0a92d726eae93bf2/SmileVue/index',
+                method:'get',
+            })
+            .then(response=>{
+                console.log(response)
+            })
+            .catch(error=>{
+                console.log(error)
+            });
+            
+        }
+    }
+</script>
+
+<style  scoped>
+    .search-bar{
+        height:2.2rem;
+        background-color: #e5017d;
+        line-height: 2.2rem;
+        overflow: hidden;
+    }
+    .search-input{
+        width:100%;
+        height: 1.3rem;
+        border-top:0px;
+        border-left:0px;
+        border-right:0px;
+        border-bottom:1px solid #fff !important;
+        background-color: #e5017d;
+        color:#fff;
+    }
+    .location-icon{
+        padding-top:.2rem;
+        padding-left:.3rem;
+    }
+    .swiper-area{
+        clear:both;
+        max-height:15rem;
+        overflow: hidden;
+    }
+</style>
